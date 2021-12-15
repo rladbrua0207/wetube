15eb85db7e0394737e8db80793cf92139932ce12
rladbrua0207 <rladbrua0207@gmail.com>
2021-12-16 01:24:24 +0900

    cors test

--- src/server.js
+++ src/server.js
@@ -37,13 +37,6 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-  next();
-});
-
-app.use((req, res, next) => {
   req.sessionStore.all((error, sessions) => {
     //console.log(sessions);
     next();
