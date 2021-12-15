3fd43eb37b13c113dc8223867c0a45bebcb6ca69
rladbrua0207 <rladbrua0207@gmail.com>
2021-12-16 00:42:28 +0900

    cors

--- src/server.js
+++ src/server.js
@@ -16,9 +16,8 @@ const app = express();
 const logger = morgan("dev");
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
 
